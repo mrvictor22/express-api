382669d8730e47489bb18cca8ef098950d3fb0fa
alfons22 <vc70383@hotmail.com>
2022-09-13 17:02:57 -0600

    QR Update Https

--- app/Http/Controllers/RutasController.php
+++ app/Http/Controllers/RutasController.php
@@ -348,7 +348,7 @@ class RutasController extends Controller
           $tipo_entrega = $object->mode;
           $sucursal = $object->sucursal;
           $fecha_despacho = $object->fecha_despacho;
-          $google_API = "http://chart.googleapis.com/chart?chs=390x390&cht=qr&chl=";
+          $google_API = "https://chart.googleapis.com/chart?chs=390x390&cht=qr&chl=";
         }
         return view('rutas.qr-print',compact('ruta_id','telefono','google_API','sucursal','fecha_despacho', 'id_guia','destino','nombre_contact','tipo_entrega'));
     }
