b87eaa8362be4530f3451ce32ab401164711dc05
Victor Alfonso <vc70383@hotmail.com>
2023-04-12 17:13:10 -0600

    assets

--- public/assets/js/pages/form-pickers.init.js
+++ public/assets/js/pages/form-pickers.init.js
@@ -1,1 +1,199 @@
-(()=>{var r=document.querySelectorAll(".classic-colorpicker");r&&Array.from(r).forEach((function(){Pickr.create({el:".classic-colorpicker",theme:"classic",default:"#405189",swatches:["rgba(244, 67, 54, 1)","rgba(233, 30, 99, 0.95)","rgba(156, 39, 176, 0.9)","rgba(103, 58, 183, 0.85)","rgba(63, 81, 181, 0.8)","rgba(33, 150, 243, 0.75)","rgba(3, 169, 244, 0.7)","rgba(0, 188, 212, 0.7)","rgba(0, 150, 136, 0.75)","rgba(76, 175, 80, 0.8)","rgba(139, 195, 74, 0.85)","rgba(205, 220, 57, 0.9)","rgba(255, 235, 59, 0.95)","rgba(255, 193, 7, 1)"],components:{preview:!0,opacity:!0,hue:!0,interaction:{hex:!0,rgba:!0,hsva:!0,input:!0,clear:!0,save:!0}}})}));var e=document.querySelectorAll(".monolith-colorpicker");e&&Array.from(e).forEach((function(){Pickr.create({el:".monolith-colorpicker",theme:"monolith",default:"#0ab39c",swatches:["rgba(244, 67, 54, 1)","rgba(233, 30, 99, 0.95)","rgba(156, 39, 176, 0.9)","rgba(103, 58, 183, 0.85)","rgba(63, 81, 181, 0.8)","rgba(33, 150, 243, 0.75)","rgba(3, 169, 244, 0.7)"],defaultRepresentation:"HEXA",components:{preview:!0,opacity:!0,hue:!0,interaction:{hex:!1,rgba:!1,hsva:!1,input:!0,clear:!0,save:!0}}})}));var a=document.querySelectorAll(".nano-colorpicker");a&&Array.from(a).forEach((function(){Pickr.create({el:".nano-colorpicker",theme:"nano",default:"#3577f1",swatches:["rgba(244, 67, 54, 1)","rgba(233, 30, 99, 0.95)","rgba(156, 39, 176, 0.9)","rgba(103, 58, 183, 0.85)","rgba(63, 81, 181, 0.8)","rgba(33, 150, 243, 0.75)","rgba(3, 169, 244, 0.7)"],defaultRepresentation:"HEXA",components:{preview:!0,opacity:!0,hue:!0,interaction:{hex:!1,rgba:!1,hsva:!1,input:!0,clear:!0,save:!0}}})}));var o=document.querySelectorAll(".colorpicker-demo");o&&Array.from(o).forEach((function(){Pickr.create({el:".colorpicker-demo",theme:"monolith",default:"#405189",components:{preview:!0,interaction:{clear:!0,save:!0}}})}));var c=document.querySelectorAll(".colorpicker-opacity-hue");c&&Array.from(c).forEach((function(){Pickr.create({el:".colorpicker-opacity-hue",theme:"monolith",default:"#0ab39c",components:{preview:!0,opacity:!0,hue:!0,interaction:{clear:!0,save:!0}}})}));var t=document.querySelectorAll(".colorpicker-switch");t&&Array.from(t).forEach((function(){Pickr.create({el:".colorpicker-switch",theme:"monolith",default:"#3577f1",swatches:["rgba(244, 67, 54, 1)","rgba(233, 30, 99, 0.95)","rgba(156, 39, 176, 0.9)","rgba(103, 58, 183, 0.85)","rgba(63, 81, 181, 0.8)","rgba(33, 150, 243, 0.75)","rgba(3, 169, 244, 0.7)"],components:{preview:!0,opacity:!0,hue:!0,interaction:{clear:!0,save:!0}}})}));var i=document.querySelectorAll(".colorpicker-input");i&&Array.from(i).forEach((function(){Pickr.create({el:".colorpicker-input",theme:"monolith",default:"#f7b84b",swatches:["rgba(244, 67, 54, 1)","rgba(233, 30, 99, 0.95)","rgba(156, 39, 176, 0.9)","rgba(103, 58, 183, 0.85)","rgba(63, 81, 181, 0.8)","rgba(33, 150, 243, 0.75)","rgba(3, 169, 244, 0.7)"],components:{preview:!0,opacity:!0,hue:!0,interaction:{input:!0,clear:!0,save:!0}}})}));var n=document.querySelectorAll(".colorpicker-format");n&&Array.from(n).forEach((function(){Pickr.create({el:".colorpicker-format",theme:"monolith",default:"#f06548",swatches:["rgba(244, 67, 54, 1)","rgba(233, 30, 99, 0.95)","rgba(156, 39, 176, 0.9)","rgba(103, 58, 183, 0.85)","rgba(63, 81, 181, 0.8)","rgba(33, 150, 243, 0.75)","rgba(3, 169, 244, 0.7)"],components:{preview:!0,opacity:!0,hue:!0,interaction:{hex:!0,rgba:!0,hsva:!0,input:!0,clear:!0,save:!0}}})}))})();
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*************************************************!*\
+  !*** ./resources/js/pages/form-pickers.init.js ***!
+  \*************************************************/
+/*
+Template Name: Velzon - Admin & Dashboard Template
+Author: Themesbrand
+Website: https://Themesbrand.com/
+Contact: Themesbrand@gmail.com
+File: Form pickers Js File
+*/
+// colorpickers
+// classic color picker
+var classicPickrDemo = document.querySelectorAll(".classic-colorpicker");
+if (classicPickrDemo) Array.from(classicPickrDemo).forEach(function () {
+  Pickr.create({
+    el: ".classic-colorpicker",
+    theme: "classic",
+    // or 'monolith', or 'nano'
+    "default": "#405189",
+    swatches: ["rgba(244, 67, 54, 1)", "rgba(233, 30, 99, 0.95)", "rgba(156, 39, 176, 0.9)", "rgba(103, 58, 183, 0.85)", "rgba(63, 81, 181, 0.8)", "rgba(33, 150, 243, 0.75)", "rgba(3, 169, 244, 0.7)", "rgba(0, 188, 212, 0.7)", "rgba(0, 150, 136, 0.75)", "rgba(76, 175, 80, 0.8)", "rgba(139, 195, 74, 0.85)", "rgba(205, 220, 57, 0.9)", "rgba(255, 235, 59, 0.95)", "rgba(255, 193, 7, 1)"],
+    components: {
+      // Main components
+      preview: true,
+      opacity: true,
+      hue: true,
+      // Input / output Options
+      interaction: {
+        hex: true,
+        rgba: true,
+        hsva: true,
+        input: true,
+        clear: true,
+        save: true
+      }
+    }
+  });
+}); // monolith color picker
+
+var monolithColorPickr = document.querySelectorAll(".monolith-colorpicker");
+if (monolithColorPickr) Array.from(monolithColorPickr).forEach(function () {
+  Pickr.create({
+    el: ".monolith-colorpicker",
+    theme: "monolith",
+    "default": "#0ab39c",
+    swatches: ["rgba(244, 67, 54, 1)", "rgba(233, 30, 99, 0.95)", "rgba(156, 39, 176, 0.9)", "rgba(103, 58, 183, 0.85)", "rgba(63, 81, 181, 0.8)", "rgba(33, 150, 243, 0.75)", "rgba(3, 169, 244, 0.7)"],
+    defaultRepresentation: "HEXA",
+    components: {
+      // Main components
+      preview: true,
+      opacity: true,
+      hue: true,
+      // Input / output Options
+      interaction: {
+        hex: false,
+        rgba: false,
+        hsva: false,
+        input: true,
+        clear: true,
+        save: true
+      }
+    }
+  });
+}); // nano color picker
+
+var nanoColorPickr = document.querySelectorAll(".nano-colorpicker");
+if (nanoColorPickr) Array.from(nanoColorPickr).forEach(function () {
+  Pickr.create({
+    el: ".nano-colorpicker",
+    theme: "nano",
+    "default": "#3577f1",
+    swatches: ["rgba(244, 67, 54, 1)", "rgba(233, 30, 99, 0.95)", "rgba(156, 39, 176, 0.9)", "rgba(103, 58, 183, 0.85)", "rgba(63, 81, 181, 0.8)", "rgba(33, 150, 243, 0.75)", "rgba(3, 169, 244, 0.7)"],
+    defaultRepresentation: "HEXA",
+    components: {
+      // Main components
+      preview: true,
+      opacity: true,
+      hue: true,
+      // Input / output Options
+      interaction: {
+        hex: false,
+        rgba: false,
+        hsva: false,
+        input: true,
+        clear: true,
+        save: true
+      }
+    }
+  });
+}); // demo color picker
+
+var demoColorPickr = document.querySelectorAll(".colorpicker-demo");
+if (demoColorPickr) Array.from(demoColorPickr).forEach(function () {
+  Pickr.create({
+    el: ".colorpicker-demo",
+    theme: "monolith",
+    "default": "#405189",
+    components: {
+      // Main components
+      preview: true,
+      // Input / output Options
+      interaction: {
+        clear: true,
+        save: true
+      }
+    }
+  });
+}); // color picker opacity & hue
+
+var opacityHueColorPickr = document.querySelectorAll(".colorpicker-opacity-hue");
+if (opacityHueColorPickr) Array.from(opacityHueColorPickr).forEach(function () {
+  Pickr.create({
+    el: ".colorpicker-opacity-hue",
+    theme: "monolith",
+    "default": "#0ab39c",
+    components: {
+      // Main components
+      preview: true,
+      opacity: true,
+      hue: true,
+      // Input / output Options
+      interaction: {
+        clear: true,
+        save: true
+      }
+    }
+  });
+}); // color picker swatches
+
+var swatcherColorPickr = document.querySelectorAll(".colorpicker-switch");
+if (swatcherColorPickr) Array.from(swatcherColorPickr).forEach(function () {
+  Pickr.create({
+    el: ".colorpicker-switch",
+    theme: "monolith",
+    "default": "#3577f1",
+    swatches: ["rgba(244, 67, 54, 1)", "rgba(233, 30, 99, 0.95)", "rgba(156, 39, 176, 0.9)", "rgba(103, 58, 183, 0.85)", "rgba(63, 81, 181, 0.8)", "rgba(33, 150, 243, 0.75)", "rgba(3, 169, 244, 0.7)"],
+    components: {
+      // Main components
+      preview: true,
+      opacity: true,
+      hue: true,
+      // Input / output Options
+      interaction: {
+        clear: true,
+        save: true
+      }
+    }
+  });
+}); // color picker input
+
+var inputColorPickr = document.querySelectorAll(".colorpicker-input");
+if (inputColorPickr) Array.from(inputColorPickr).forEach(function () {
+  Pickr.create({
+    el: ".colorpicker-input",
+    theme: "monolith",
+    "default": "#f7b84b",
+    swatches: ["rgba(244, 67, 54, 1)", "rgba(233, 30, 99, 0.95)", "rgba(156, 39, 176, 0.9)", "rgba(103, 58, 183, 0.85)", "rgba(63, 81, 181, 0.8)", "rgba(33, 150, 243, 0.75)", "rgba(3, 169, 244, 0.7)"],
+    components: {
+      // Main components
+      preview: true,
+      opacity: true,
+      hue: true,
+      // Input / output Options
+      interaction: {
+        input: true,
+        clear: true,
+        save: true
+      }
+    }
+  });
+}); // color picker Format
+
+var formatColorPickr = document.querySelectorAll(".colorpicker-format");
+if (formatColorPickr) Array.from(formatColorPickr).forEach(function () {
+  Pickr.create({
+    el: ".colorpicker-format",
+    theme: "monolith",
+    "default": "#f06548",
+    swatches: ["rgba(244, 67, 54, 1)", "rgba(233, 30, 99, 0.95)", "rgba(156, 39, 176, 0.9)", "rgba(103, 58, 183, 0.85)", "rgba(63, 81, 181, 0.8)", "rgba(33, 150, 243, 0.75)", "rgba(3, 169, 244, 0.7)"],
+    components: {
+      // Main components
+      preview: true,
+      opacity: true,
+      hue: true,
+      // Input / output Options
+      interaction: {
+        hex: true,
+        rgba: true,
+        hsva: true,
+        input: true,
+        clear: true,
+        save: true
+      }
+    }
+  });
+});
+/******/ })()
+;
--- public/assets/js/pages/range-sliders.init.js
+++ public/assets/js/pages/range-sliders.init.js
@@ -1,1 +1,420 @@
-(()=>{var e=document.querySelectorAll("[data-rangeslider]");e&&Array.from(e).forEach((function(e){noUiSlider.create(e,{start:127,connect:"lower",range:{min:0,max:255}})}));var n=document.querySelectorAll("[data-multielement]");n&&Array.from(n).forEach((function(e){noUiSlider.create(e,{start:[20,80],connect:!0,range:{min:0,max:100}})}));var t=document.getElementById("result"),r=document.getElementsByClassName("sliders"),i=[0,0,0];r&&Array.from([].slice.call(r)).forEach((function(e,n){noUiSlider.create(e,{start:127,connect:[!0,!1],orientation:"vertical",range:{min:0,max:255},format:wNumb({decimals:0})}),e.noUiSlider.on("update",(function(){i[n]=e.noUiSlider.get();var r="rgb("+i.join(",")+")";t.style.background=r,t.style.color=r}))}));for(var o=document.getElementById("input-select"),a=-20;a<=40;a++){var l=document.createElement("option");l.text=a,l.value=a,o.appendChild(l)}var d=document.getElementById("html5");d&&noUiSlider.create(d,{start:[10,30],connect:!0,range:{min:-20,max:40}});var c=document.getElementById("input-number");c&&d&&(d.noUiSlider.on("update",(function(e,n){var t=e[n];n?c.value=t:o.value=Math.round(t)})),o.addEventListener("change",(function(){d.noUiSlider.set([this.value,null])})),c.addEventListener("change",(function(){d.noUiSlider.set([null,this.value])})));var m=document.getElementById("nonlinear");m&&noUiSlider.create(m,{connect:!0,behaviour:"tap",start:[500,4e3],range:{min:[0],"10%":[500,500],"50%":[4e3,1e3],max:[1e4]}});var u=[document.getElementById("lower-value"),document.getElementById("upper-value")];m.noUiSlider.on("update",(function(e,n,t,r,i){u[n].innerHTML=e[n]+", "+i[n].toFixed(2)+"%"}));var s=!1,g=[60,80],f=document.getElementById("slider1"),v=document.getElementById("slider2"),S=document.getElementById("lockbutton"),y=document.getElementById("slider1-span"),U=document.getElementById("slider2-span");function p(e,n){if(s){var t=f===n?0:1;e-=g[t?0:1]-g[t],n.noUiSlider.set(e)}}if(S&&S.addEventListener("click",(function(){s=!s,this.textContent=s?"unlock":"lock"})),noUiSlider.create(f,{start:60,animate:!1,range:{min:50,max:100}}),noUiSlider.create(v,{start:80,animate:!1,range:{min:50,max:100}}),f&&v){var E=function(){g=[Number(f.noUiSlider.get()),Number(v.noUiSlider.get())]};f.noUiSlider.on("update",(function(e,n){y.innerHTML=e[n]})),v.noUiSlider.on("update",(function(e,n){U.innerHTML=e[n]})),f.noUiSlider.on("change",E),v.noUiSlider.on("change",E),f.noUiSlider.on("slide",(function(e,n){p(e[n],v)})),v.noUiSlider.on("slide",(function(e,n){p(e[n],f)}))}var h=document.getElementById("slider-merging-tooltips");h&&(noUiSlider.create(h,{start:[20,75],connect:!0,tooltips:[!0,!0],range:{min:0,max:100}}),function(e,n,t){var r="rtl"===getComputedStyle(e).direction,i="rtl"===e.noUiSlider.options.direction,o="vertical"===e.noUiSlider.options.orientation,a=e.noUiSlider.getTooltips(),l=e.noUiSlider.getOrigins();Array.from(a).forEach((function(e,n){e&&l[n].appendChild(e)})),e&&e.noUiSlider.on("update",(function(e,l,d,c,m){var u=[[]],s=[[]],g=[[]],f=0;a[0]&&(u[0][0]=0,s[0][0]=m[0],g[0][0]=e[0]);for(var v=1;v<m.length;v++)(!a[v]||m[v]-m[v-1]>n)&&(u[++f]=[],g[f]=[],s[f]=[]),a[v]&&(u[f].push(v),g[f].push(e[v]),s[f].push(m[v]));Array.from(u).forEach((function(e,n){for(var l=e.length,d=0;d<l;d++){var c=e[d];if(d===l-1){var m=0;Array.from(s[n]).forEach((function(e){m+=1e3-e}));var u=o?"bottom":"right",f=i?0:l-1,v=1e3-s[n][f];m=(r&&!o?100:0)+m/l-v,a[c].innerHTML=g[n].join(t),a[c].style.display="block",a[c].style[u]=m+"%"}else a[c].style.display="none"}}))}))}(h,5," - "));var B=document.getElementById("slider-hide");B&&noUiSlider.create(B,{range:{min:0,max:100},start:[20,80],tooltips:!0});var I=document.getElementById("slider-pips");I&&noUiSlider.create(I,{range:{min:0,max:100},start:[50],pips:{mode:"count",values:5}});var x=I.querySelectorAll(".noUi-value");function b(){var e=Number(this.getAttribute("data-value"));I.noUiSlider.set(e)}x&&Array.from(x).forEach((function(e){e.style.cursor="pointer",e.addEventListener("click",b)}));var A=document.getElementById("slider-color");A&&noUiSlider.create(A,{start:[4e3,8e3,12e3,16e3],connect:[!1,!0,!0,!0,!0],range:{min:[2e3],max:[2e4]}});var L=A.querySelectorAll(".noUi-connect"),k=["c-1-color","c-2-color","c-3-color","c-4-color","c-5-color"];a=0;Array.from(L).forEach((function(e){e.classList.add(k[a]),a++}));var N=document.getElementById("slider-toggle");N&&(noUiSlider.create(N,{orientation:"vertical",start:0,range:{min:[0,1],max:1},format:wNumb({decimals:0})}),N.noUiSlider.on("update",(function(e,n){"1"===e[n]?N.classList.add("off"):N.classList.remove("off")})));var C=document.getElementById("soft");C&&(noUiSlider.create(C,{start:50,range:{min:0,max:100},pips:{mode:"values",values:[20,80],density:4}}),C.noUiSlider.on("change",(function(e,n){e[n]<20?C.noUiSlider.set(20):e[n]>80&&C.noUiSlider.set(80)})))})();
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**************************************************!*\
+  !*** ./resources/js/pages/range-sliders.init.js ***!
+  \**************************************************/
+/*
+Template Name: Velzon - Admin & Dashboard Template
+Author: Themesbrand
+Website: https://Themesbrand.com/
+Contact: Themesbrand@gmail.com
+File: Range sliders Js File
+*/
+
+/*********************
+  basic example
+**********************/
+var sliderColorScheme = document.querySelectorAll('[data-rangeslider]');
+if (sliderColorScheme) Array.from(sliderColorScheme).forEach(function (slider) {
+  noUiSlider.create(slider, {
+    start: 127,
+    connect: 'lower',
+    range: {
+      'min': 0,
+      'max': 255
+    }
+  });
+});
+/*********************
+  multi range handle
+**********************/
+
+var multielementslider = document.querySelectorAll('[data-multielement]');
+if (multielementslider) Array.from(multielementslider).forEach(function (slider) {
+  noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    range: {
+      'min': 0,
+      'max': 100
+    }
+  });
+});
+/*********************
+  Colorpicker
+**********************/
+
+var resultElement = document.getElementById('result');
+var sliders = document.getElementsByClassName('sliders');
+var colors = [0, 0, 0];
+if (sliders) Array.from([].slice.call(sliders)).forEach(function (slider, index) {
+  noUiSlider.create(slider, {
+    start: 127,
+    connect: [true, false],
+    orientation: "vertical",
+    range: {
+      'min': 0,
+      'max': 255
+    },
+    format: wNumb({
+      decimals: 0
+    })
+  }); // Bind the color changing function to the update event.
+
+  slider.noUiSlider.on('update', function () {
+    colors[index] = slider.noUiSlider.get();
+    var color = 'rgb(' + colors.join(',') + ')';
+    resultElement.style.background = color;
+    resultElement.style.color = color;
+  });
+});
+/*********************
+  Using HTML5 input elements
+**********************/
+
+var select = document.getElementById('input-select'); // Append the option elements
+
+for (var i = -20; i <= 40; i++) {
+  var option = document.createElement("option");
+  option.text = i;
+  option.value = i;
+  select.appendChild(option);
+}
+
+var html5Slider = document.getElementById('html5');
+if (html5Slider) noUiSlider.create(html5Slider, {
+  start: [10, 30],
+  connect: true,
+  range: {
+    'min': -20,
+    'max': 40
+  }
+});
+var inputNumber = document.getElementById('input-number');
+
+if (inputNumber && html5Slider) {
+  html5Slider.noUiSlider.on('update', function (values, handle) {
+    var value = values[handle];
+
+    if (handle) {
+      inputNumber.value = value;
+    } else {
+      select.value = Math.round(value);
+    }
+  });
+  select.addEventListener('change', function () {
+    html5Slider.noUiSlider.set([this.value, null]);
+  });
+  inputNumber.addEventListener('change', function () {
+    html5Slider.noUiSlider.set([null, this.value]);
+  });
+}
+/*********************
+  Non linear slider
+**********************/
+
+
+var nonLinearSlider = document.getElementById('nonlinear');
+if (nonLinearSlider) noUiSlider.create(nonLinearSlider, {
+  connect: true,
+  behaviour: 'tap',
+  start: [500, 4000],
+  range: {
+    // Starting at 500, step the value by 500,
+    // until 4000 is reached. From there, step by 1000.
+    'min': [0],
+    '10%': [500, 500],
+    '50%': [4000, 1000],
+    'max': [10000]
+  }
+});
+var nodes = [document.getElementById('lower-value'), // 0
+document.getElementById('upper-value') // 1
+]; // Display the slider value and how far the handle moved
+// from the left edge of the slider.
+
+nonLinearSlider.noUiSlider.on('update', function (values, handle, unencoded, isTap, positions) {
+  nodes[handle].innerHTML = values[handle] + ', ' + positions[handle].toFixed(2) + '%';
+});
+/*********************
+  Locking sliders together
+**********************/
+
+var lockedState = false;
+var lockedSlider = false;
+var lockedValues = [60, 80];
+var slider1 = document.getElementById('slider1');
+var slider2 = document.getElementById('slider2');
+var lockButton = document.getElementById('lockbutton');
+var slider1Value = document.getElementById('slider1-span');
+var slider2Value = document.getElementById('slider2-span'); // When the button is clicked, the locked state is inverted.
+
+if (lockButton) lockButton.addEventListener('click', function () {
+  lockedState = !lockedState;
+  this.textContent = lockedState ? 'unlock' : 'lock';
+});
+
+function crossUpdate(value, slider) {
+  // If the sliders aren't interlocked, don't
+  // cross-update.
+  if (!lockedState) return; // Select whether to increase or decrease
+  // the other slider value.
+
+  var a = slider1 === slider ? 0 : 1; // Invert a
+
+  var b = a ? 0 : 1; // Offset the slider value.
+
+  value -= lockedValues[b] - lockedValues[a]; // Set the value
+
+  slider.noUiSlider.set(value);
+}
+
+noUiSlider.create(slider1, {
+  start: 60,
+  // Disable animation on value-setting,
+  // so the sliders respond immediately.
+  animate: false,
+  range: {
+    min: 50,
+    max: 100
+  }
+});
+noUiSlider.create(slider2, {
+  start: 80,
+  animate: false,
+  range: {
+    min: 50,
+    max: 100
+  }
+});
+
+if (slider1 && slider2) {
+  var setLockedValues = function setLockedValues() {
+    lockedValues = [Number(slider1.noUiSlider.get()), Number(slider2.noUiSlider.get())];
+  };
+
+  slider1.noUiSlider.on('update', function (values, handle) {
+    slider1Value.innerHTML = values[handle];
+  });
+  slider2.noUiSlider.on('update', function (values, handle) {
+    slider2Value.innerHTML = values[handle];
+  });
+  slider1.noUiSlider.on('change', setLockedValues);
+  slider2.noUiSlider.on('change', setLockedValues);
+  slider1.noUiSlider.on('slide', function (values, handle) {
+    crossUpdate(values[handle], slider2);
+  });
+  slider2.noUiSlider.on('slide', function (values, handle) {
+    crossUpdate(values[handle], slider1);
+  });
+}
+/*********************
+    mergingTooltipSlider
+**********************/
+
+
+var mergingTooltipSlider = document.getElementById('slider-merging-tooltips');
+
+if (mergingTooltipSlider) {
+  noUiSlider.create(mergingTooltipSlider, {
+    start: [20, 75],
+    connect: true,
+    tooltips: [true, true],
+    range: {
+      'min': 0,
+      'max': 100
+    }
+  });
+  mergeTooltips(mergingTooltipSlider, 5, ' - ');
+}
+/**
+ * @param slider HtmlElement with an initialized slider
+ * @param threshold Minimum proximity (in percentages) to merge tooltips
+ * @param separator String joining tooltips
+ */
+
+
+function mergeTooltips(slider, threshold, separator) {
+  var textIsRtl = getComputedStyle(slider).direction === 'rtl';
+  var isRtl = slider.noUiSlider.options.direction === 'rtl';
+  var isVertical = slider.noUiSlider.options.orientation === 'vertical';
+  var tooltips = slider.noUiSlider.getTooltips();
+  var origins = slider.noUiSlider.getOrigins(); // Move tooltips into the origin element. The default stylesheet handles this.
+
+  Array.from(tooltips).forEach(function (tooltip, index) {
+    if (tooltip) {
+      origins[index].appendChild(tooltip);
+    }
+  });
+  if (slider) slider.noUiSlider.on('update', function (values, handle, unencoded, tap, positions) {
+    var pools = [[]];
+    var poolPositions = [[]];
+    var poolValues = [[]];
+    var atPool = 0; // Assign the first tooltip to the first pool, if the tooltip is configured
+
+    if (tooltips[0]) {
+      pools[0][0] = 0;
+      poolPositions[0][0] = positions[0];
+      poolValues[0][0] = values[0];
+    }
+
+    for (var i = 1; i < positions.length; i++) {
+      if (!tooltips[i] || positions[i] - positions[i - 1] > threshold) {
+        atPool++;
+        pools[atPool] = [];
+        poolValues[atPool] = [];
+        poolPositions[atPool] = [];
+      }
+
+      if (tooltips[i]) {
+        pools[atPool].push(i);
+        poolValues[atPool].push(values[i]);
+        poolPositions[atPool].push(positions[i]);
+      }
+    }
+
+    Array.from(pools).forEach(function (pool, poolIndex) {
+      var handlesInPool = pool.length;
+
+      for (var j = 0; j < handlesInPool; j++) {
+        var handleNumber = pool[j];
+
+        if (j === handlesInPool - 1) {
+          var offset = 0;
+          Array.from(poolPositions[poolIndex]).forEach(function (value) {
+            offset += 1000 - value;
+          });
+          var direction = isVertical ? 'bottom' : 'right';
+          var last = isRtl ? 0 : handlesInPool - 1;
+          var lastOffset = 1000 - poolPositions[poolIndex][last];
+          offset = (textIsRtl && !isVertical ? 100 : 0) + offset / handlesInPool - lastOffset; // Center this tooltip over the affected handles
+
+          tooltips[handleNumber].innerHTML = poolValues[poolIndex].join(separator);
+          tooltips[handleNumber].style.display = 'block';
+          tooltips[handleNumber].style[direction] = offset + '%';
+        } else {
+          // Hide this tooltip
+          tooltips[handleNumber].style.display = 'none';
+        }
+      }
+    });
+  });
+}
+/*********************
+    tooltip
+**********************/
+
+
+var hidingTooltipSlider = document.getElementById('slider-hide');
+if (hidingTooltipSlider) noUiSlider.create(hidingTooltipSlider, {
+  range: {
+    min: 0,
+    max: 100
+  },
+  start: [20, 80],
+  tooltips: true
+});
+/*********************
+    pipe - scale
+**********************/
+
+var pipsSlider = document.getElementById('slider-pips');
+if (pipsSlider) noUiSlider.create(pipsSlider, {
+  range: {
+    min: 0,
+    max: 100
+  },
+  start: [50],
+  pips: {
+    mode: 'count',
+    values: 5
+  }
+});
+var pips = pipsSlider.querySelectorAll('.noUi-value');
+
+function clickOnPip() {
+  var value = Number(this.getAttribute('data-value'));
+  pipsSlider.noUiSlider.set(value);
+}
+
+if (pips) Array.from(pips).forEach(function (ele) {
+  // For this example. Do this in CSS!
+  ele.style.cursor = 'pointer';
+  ele.addEventListener('click', clickOnPip);
+});
+/*********************
+  Colored Connect Elements
+**********************/
+
+var slider = document.getElementById('slider-color');
+if (slider) noUiSlider.create(slider, {
+  start: [4000, 8000, 12000, 16000],
+  connect: [false, true, true, true, true],
+  range: {
+    'min': [2000],
+    'max': [20000]
+  }
+});
+var connect = slider.querySelectorAll('.noUi-connect');
+var classes = ['c-1-color', 'c-2-color', 'c-3-color', 'c-4-color', 'c-5-color'];
+var i = 0;
+Array.from(connect).forEach(function (ele) {
+  ele.classList.add(classes[i]);
+  i++;
+});
+/*********************
+    toggle slider
+**********************/
+
+var toggleSlider = document.getElementById('slider-toggle');
+
+if (toggleSlider) {
+  noUiSlider.create(toggleSlider, {
+    orientation: "vertical",
+    start: 0,
+    range: {
+      'min': [0, 1],
+      'max': 1
+    },
+    format: wNumb({
+      decimals: 0
+    })
+  });
+  toggleSlider.noUiSlider.on('update', function (values, handle) {
+    if (values[handle] === '1') {
+      toggleSlider.classList.add('off');
+    } else {
+      toggleSlider.classList.remove('off');
+    }
+  });
+}
+/*********************
+    Soft limits
+**********************/
+
+
+var softSlider = document.getElementById('soft');
+
+if (softSlider) {
+  noUiSlider.create(softSlider, {
+    start: 50,
+    range: {
+      min: 0,
+      max: 100
+    },
+    pips: {
+      mode: 'values',
+      values: [20, 80],
+      density: 4
+    }
+  });
+  softSlider.noUiSlider.on('change', function (values, handle) {
+    if (values[handle] < 20) {
+      softSlider.noUiSlider.set(20);
+    } else if (values[handle] > 80) {
+      softSlider.noUiSlider.set(80);
+    }
+  });
+}
+/******/ })()
+;
