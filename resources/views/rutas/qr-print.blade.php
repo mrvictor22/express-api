1409fdb3eafd11ecf5351a2a9a73ca854271f4c2
Victor Alfonso <vc70383@hotmail.com>
2023-04-19 20:30:40 -0600

    EAPI-9 Diseño 1

--- resources/views/rutas/qr-print.blade.php
+++ resources/views/rutas/qr-print.blade.php
@@ -20,13 +20,6 @@
     @endcomponent
 
 <style>
-    table, th, td {
-        border: 4px solid black;
-        border-collapse: collapse;
-    }
-    #qr-iframe{
-        border-color: white;
-    }
     .card-body{
         display: flex;
         justify-content: center;
@@ -41,73 +34,124 @@
             font-size: 30px;
             /*padding-left: 25%;*/
         }
-        table {
-            border: 1px solid black;
-            border-collapse: collapse;
-        }
-        th, td {
-            border: 1px solid black;
-            border-collapse: collapse;
-            justify-content: center;
-            align-items: center;
-        }
-        #qr-iframe{
-            border-color: white;
-        }
         .hidden-print {
             display: none !important;
         }
     }
 
+    /* Estilos para el contenido de la etiqueta */
+    #etiqueta {
+        width: 100%;
+        padding: 0.1cm;
+        font-size: 12px;
+        line-height: 1.2;
+        border: 1px solid #000;
+        text-align: center;
+    }
 
+    /* Estilos para el QR code */
+    /*#qr {*/
+    /*    margin-top: 0.5cm;*/
+    /*    margin-bottom: 0.5cm;*/
+    /*    display: inline-block;*/
+    /*    border: 1px solid #000;*/
+    /*    padding: 0.5cm;*/
+    /*    width: 30%;*/
+    /*}*/
+    h3 {
+        text-align: center;
+    }
+
+    hr {
+        border-top: 1px solid #bbb;
+        width: 80%;
+        margin: 20px auto;
+    }
+    .emisor {
+        width: 50%;
+        float: left;
+    }
+
+    .remitente {
+        width: 45%;
+        float: right;
+    }
+    .productos{
+        width: 45%;
+        float: left;
+    }
+    .qr{
+        width: 45%;
+        float: right;
+    }
 </style>
 
 <div class="card-body" id="printableArea" >
     <div class="row">
         @if( $id_guia)
 
-            <table class="tg" style="border: 4px solid black;border-collapse: collapse;">
-                <thead>
-                <tr style="border: 4px solid black; border-collapse: collapse;">
-                    <td colspan="2" style="text-align: center; border: 4px solid black;border-collapse: collapse;">
-                        <img src="{{ URL::asset('assets/images/ssss.png')}}"  width="255" ></td>
-                </tr>
-                <tr style="border: 4px solid black;border-collapse: collapse;">
+            <div id="etiqueta">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md text-center">
+                            <img src="{{ URL::asset('assets/images/ssss.png')}}"  width="255" >
+                        </div>
+                        <hr>
+                    </div>
+                    <div class="row">
+                        <div class="col-md emisor">
+                            <h3>Información del Emisor</h3>
+                            <!-- Aquí va la información del emisor -->
+                            <p>Nombre: [REEMPLAZAR]</p>
+                            <p>Dirección: [REEMPLAZAR]</p>
+                            <p>Teléfono: [REEMPLAZAR]</p>
+                            <p>SUCURSAL: {{$sucursal}}</p>
+                            <p>FECHA DESPACHO: {{$fecha_despacho}}</p>
+                        </div>
+                        <div class="col-md remitente">
+                            <h3>Información del Remitente</h3>
+                            <!-- Aquí va la información del remitente -->
+                            <p>Nombre: {{$nombre_contact}}</p>
+                            <p>Dirección: {{$destino}}</p>
+                            <p>Teléfono: {{$telefono}}</p>
+                        </div>
+                    </div>
+                    <h3>Información del Paquete</h3>
+                    <h1>ID de guía: {{$id_guia}}</h1>
+                    <div class="row">
+                        <div class="col-sm productos">
+                            <!-- Aquí iría la información de los productos -->
+                            <p><strong>Información de los productos:</strong></p>
+                            <p>Producto 1: [REEMPLAZAR]</p>
+                            <p>Producto 2: [REEMPLAZAR]</p>
+                            <p>Producto 3: [REEMPLAZAR]</p>
+                            <hr>
+                        </div>
+                        <div class="col-sm qr">
+                                <!-- QR code con el ID de guía -->
+                                <div id="qr">
+                                    <img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={{$id_guia}}">
+                                </div>
 
-                    <td class="tg-0lax" style="border: 4px solid black;border-collapse: collapse; width: 50%">
-                        <h3>Guia:</h3>
-                        <ul>
-                            <li>Numero de guia: {{$id_guia}}  </li>
-                            <li>Sucursal: {{$sucursal}}  </li>
-                            <li>Fecha despacho: {{$fecha_despacho}}  </li>
-                        </ul>
-                        <h3>Destino:</h3>
-                       <ul>
+                        </div>
 
-                           <li>Nombre de contacto: {{$nombre_contact}}  </li>
-                           <li>Telefono: {{$telefono}}  </li>
-                           <li>Dirección:{{$destino}}</li>
+                </div>
 
-                       </ul>
-                    </td>
-                    <td class="tg-0lax" style="border: 0px solid black;border-collapse: collapse; justify-content: center; align-items: center; display: flex">
 
-                        <iframe width="300px" height="300px" src="{{$google_API.$id_guia }}" title="QR" id="qr-iframe"></iframe>
 
-                    </td>
-                </tr>
+            </div>
 
-                </thead>
-            </table>
+
         @endif
+                <div class="row">
+                    <input class="hidden-print" type="button" onclick="printableDiv('printableArea')" value="Imprimir etiqueta" />
+                </div>
     </div>
 
 </div>
     <br>
     <a></a>
-    <div class="row">
-        <input class="hidden-print" type="button" onclick="printableDiv('printableArea')" value="Imprimir etiqueta" />
-    </div>
+
 @endsection
 @section('script')
 <script>
