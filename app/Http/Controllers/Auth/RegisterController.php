f0fec859ba2dc9fe33254715b9f7a9853520078f
Victor Alfonso <vc70383@hotmail.com>
2023-06-21 22:42:34 -0600

    EAPI-50 Formulario de registro conn

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -57,8 +57,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'avatar' => ['required', 'image' ,'mimes:jpg,jpeg,png','max:1024'],
+            'password' => ['required', 'string', 'min:8', 'confirmed']
         ]);
     }
 
@@ -81,8 +80,9 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-            'avatar' =>  $avatarName,
+            'password' => Hash::make($data['password'])
         ]);
+
+
     }
 }
